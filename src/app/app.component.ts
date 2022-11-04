b8e50d8567f58492c5f849e0719e71710ab5f826
“naman-nagwani” <namannagwani@gmail.com>
2022-11-04 11:48:13 +0530

    UC2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls:['./app.component.scss']
+  styleUrls:['./app.component.css']
 })
 export class AppComponent {
   title = 'Helloworld';
+  imgUrl = '../assets/Bridgelabz Logo.jpeg'
 
   ngOnInit(): void {
     this.title = "Hello From Bridgelabz"
