c4a4878f4d43dd481fd1884bd666d0215b3b08c7
“naman-nagwani” <namannagwani@gmail.com>
2022-11-04 10:53:31 +0530

    UC1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Helloworld';
+
+  ngOnInit(): void {
+    this.title = "Hello From Bridgelabz"
+  }
 }
